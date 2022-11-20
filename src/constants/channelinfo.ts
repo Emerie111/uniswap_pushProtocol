4dafb7a3833da0255dbdf16837ba2003caeff50d
Emerie111 <emeriemadu00@gmail.com>
2022-11-20 13:15:21 +0100

    production update

---
+++ src/constants/channelinfo.ts
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 882,
+    channel: '0x8d657c70B6F4aB242ab80938ee3ec299EDbCb94e',
+    ipfshash: 'QmRuUFHzvc8NEorzabxos8uBrukZabtBik7CARG2eWqoqH',
+    name: 'testing network',
+    info: 'nothing important',
+    url: 'https://www.google.com',
+    icon: 'https://gateway.ipfs.io/ipfs/bafybeian56ii4nmh2yd4vjkfasr6wzlkhanaeqfvfzrctjgbeqkpgu26qq/QmRuUFHzvc8NEorzabxos8uBrukZabtBik7CARG2eWqoqH',
+  },
+  {
+    id: 883,
+    channel: '0x9601f08b9EcB981D273B72e7f33964Cb98f977fe',
+    ipfshash: 'QmRuUFHzvc8NEorzabxos8uBrukZabtBik7CARG2eWqoqH',
+    name: 'Uniswap V3 LP',
+    info: 'Uniswap V3 LP NOTIFICATIONS',
+    url: 'https://www.google.com',
+    icon: 'https://gateway.ipfs.io/ipfs/bafybeifwa73xdpo4o554p3ttx434kzelqhdvn5kyv6lqvnwg3dotkvpk4q/bafkreihm57o57h6sux6ceetgrpk4jicxjsxzw4uwqejqey7fxx47pe77ny',
+  },
+]
